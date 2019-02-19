6f5cc2ed1a0a809cf95ec6968356338bce34f278
Tristan Grovender <therealtristangrovender@gmail.com>
2019-02-18 18:19:44 -0600

    end of day progress

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,9 @@
 import React from 'react';
 import './Card.css';
+
+import CardBanner from './CardBanner';
+import CardContent from './CardContent';
+
+const CardContainer = () => {
+    <div></div>
+}
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,21 @@
 import React from 'react';
 import './Header.css';
+import HeaderContent from './HeaderContent';
+import moment from 'moment';
+
+const time = moment ();
+
+const HeaderTitle = () => {
+    return (
+        <div className = "headerWrap">
+        <div className = "title">
+        Lambda School
+    <span className = "timeStamp">
+
+    </span>
+        </div>
+        </div>
+    )
+};
+
+export default HeaderTitle;
